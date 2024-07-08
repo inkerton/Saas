6c6bf356284f0c83f19234f576e15ae4bed4ad47
Janvi <127668005+inkerton@users.noreply.github.com>
2024-07-08 10:40:29 +0000

    image slider animated with framer-motion and from scratch logic grid

--- casecobra/src/app/page.tsx
+++ casecobra/src/app/page.tsx
@@ -95,7 +95,12 @@ export default function Home() {
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
-              What our <span className="relative px-2">customers<Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" /></span> say
+              What our{' '} 
+              <span className="relative px-2">
+                customers{' '} 
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+                </span>{' '} 
+                say
             </h2>
             <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
           </div>
@@ -167,6 +172,37 @@ export default function Home() {
         <div className="pt-16">
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className='mb-12 px-6 lg:px-8'>
+            <div className="mx-auto max-w-2xl sm:text-center ">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload Your Photo and get{' '} 
+                <span className="relative px-2 bg-green-600 text-white">
+                  your own case
+                  </span> 
+                  now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img src="/arrow.png" className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0" 
+              />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img src="/horse.jpg" className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full" />
+              </div>
+
+              <Phone className="w-60 " imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+
+          <ul className=""></ul>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
